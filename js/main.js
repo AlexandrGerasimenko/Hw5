c0029b2a1eb824a8feec0875561bc936d445d20b
Aleksandr Gerasimenko <avtoelectronic1987@gmail.com>
2019-02-21 11:13:35 +0200

    1

---
+++ js/main.js
@@ -0,0 +1,5 @@
+function toggle() { document.getElementById('nav-wrapperBurger').classList.toggle("active");
+}
+
+function hide() { document.getElementById('nav-wrapperBurger').classList.remove("active");
+}
